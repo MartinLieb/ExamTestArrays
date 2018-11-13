93e7ac4297dc824ac6ccf5ddfe2b479e36d73b1d
Martin <mart73p5@edu.easj.dk>
2018-11-13 16:40:46 +0100

    fix

---
+++ src/Array.java
@@ -0,0 +1,13 @@
+public class Array {
+
+    public static void main(String[] args) {
+
+
+        int[] beerCases = new int[100];
+
+        for(int i = 0; i<beerCases.length; i++)
+            System.out.println(i + " kasser øl");
+        
+    }
+
+}
---
+++ src/IfElse.java
@@ -0,0 +1,17 @@
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        int i = 1000;
+
+        if(i < 100){
+            i++;
+
+            System.out.println(i);
+
+        }
+        else{
+            System.out.println("Glem det makker!");
+        }
+    }
+}
